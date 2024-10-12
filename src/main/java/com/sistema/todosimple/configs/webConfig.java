e3c5c376e2026771161a71af0f71c07c151b156b
mesquita <joaopedromesq123@gmail.com>
2024-10-12 14:38:41 -0300

    Create WebConfig

---
+++ src/main/java/com/sistema/todosimple/configs/webConfig.java
@@ -0,0 +1,18 @@
+package com.sistema.todosimple.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+
+@Configuration
+@EnableWebMvc
+public class webConfig implements WebMvcConfigurer {
+    
+
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+
+}
